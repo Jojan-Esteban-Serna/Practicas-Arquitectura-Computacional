611f7e732f21f3e4473d721e5594facd1c578088
Jojan-Esteban-Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2022-12-27 10:46:14 -0500

    Re thinking the third practice (it was a mess)

---
+++ 003_v2_buzzer_lcd_teclado/buzzer_lcd_teclado/buzzer_lcd_teclado.ino
@@ -0,0 +1,14 @@
+#include <EasyBuzzer.h>
+#include <AsyncTaskLib.h>
+#include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
